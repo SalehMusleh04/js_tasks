f48189d197b92e97b655eadebe80cf837cc54fbf
SalehMusleh04 <saleh04.musleh@gmail.com>
2026-09-21 15:58:55 +0300

    task2_part1

---
+++ Mon-21-9-2026/task2.js
@@ -0,0 +1,59 @@
+'use strict'
+
+let cash = 1000;
+let current_liabilities = 500;
+
+console.log(cash/current_liabilities);
+
+
+let  revenues = 1000;
+let  expenses = 500;
+
+console.log(revenues-expenses);
+
+
+let  liabilities = 1000;
+let   equity = 500;
+
+console.log(revenues+expenses);
+
+let   profit = 1000;
+let   sales = 500;
+
+console.log(revenues*expenses);
+
+let num1 = 7;
+let num2 = 9;
+let num3 = 2;
+
+console.log((num1+num2+num3)/3);
+
+let   price = 150;
+let   discount = 0.3;
+
+console.log(price-(price*discount));
+
+let age = 20;
+
+if(age > 18 && age < 30) console.log(true);
+
+let number1 = 2;
+let number2 = 3;
+
+console.log(Math.pow(number1,number2));
+
+let number_1 = 10;
+let number_2 = 4;
+
+console.log(number_1%number_2);
+
+let str = 'Welcome to Orange';
+
+console.log(str.toUpperCase());
+console.log(str.split(" ")[1].toUpperCase());
+console.log(str.replace("Welcome to " , "Hello from "));
+console.log(str.toLowerCase());
+console.log(str.length);
+
+
+
